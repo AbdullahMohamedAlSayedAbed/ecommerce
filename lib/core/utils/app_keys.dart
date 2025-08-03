e73404afe35ffc9d0ae7b7ac5aae045915d447b4
AbdullahMohamedAlSayedAbed <abedabdallah834@gmail.com>
2025-08-03 15:56:07 +0300

    chore: Remove app_keys.dart from .gitignore and add it to the project

---
+++ lib/core/utils/app_keys.dart
@@ -0,0 +1,10 @@
+abstract class AppKeys {
+  static const String clientID =
+      'AZD0AfeqixB1zVC22KNfd-P2ewjDMYZdd0Iqkx4_EkiXYheelT6OUnrcF-lXVwthi4u56WfqJjmZBMXD';
+  static const String clientSecret =
+      'EJY5zdzEa8VJyvMc4Co3L9chYQkbMhxTd8gX9BQLop0tTsAI_ET1xJNnKiCMNFbdvU2SifbVbK8OjKLF';
+
+  // Define your keys here
+  static const String someKey = 'some_key';
+  // Add other keys as needed
+}
